f4c21e98b6abfe430ae4927ba550fe357f57ce4f
Ray Epps <rayharryepps@gmail.com>
2021-09-05 15:40:55 -0600

    add iter, uid, and range functions

--- src/tests/array.test.ts
+++ src/tests/array.test.ts
@@ -1,6 +1,5 @@
-import chai from 'chai'
-
-import * as ary from '../array'
+import * as chai from 'chai'
+import * as _ from '../array'
 
 const { assert } = chai
 
@@ -9,7 +8,7 @@ test('sort uses getter', () => {
 
   const list = [{ index: 2 }, { index: 0 }, { index: 1 }]
 
-  const result = ary.sort(list, i => i.index)
+  const result = _.sort(list, i => i.index)
 
   assert.equal(result[0].index, 0)
   assert.equal(result[1].index, 1)
@@ -22,10 +21,27 @@ test('sort descending order', () => {
 
   const list = [{ index: 2 }, { index: 0 }, { index: 1 }]
 
-  const result = ary.sort(list, i => i.index, true)
+  const result = _.sort(list, i => i.index, true)
 
   assert.equal(result[0].index, 2)
   assert.equal(result[1].index, 1)
   assert.equal(result[2].index, 0)
 
+})
+
+test('range creates correct list', () => {
+  const r = _.range(0, 4)
+  const total = [...r].reduce((a, b) => a + b)
+  assert.equal(total, 10)
+  assert.equal(r[0], 0)
+  assert.equal(r[4], 4)
+})
+
+test('range creates correct list with step', () => {
+  const r = _.range(0, 10, 2)
+  const total = [...r].reduce((a, b) => a + b)
+  assert.equal(total, 30)
+  assert.equal(r[0], 0)
+  assert.equal(r[4], 8)
+  assert.equal(r[5], 10)
 })
