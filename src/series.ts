9b4dc8372e38db98a5e297142c28597a0723eda6
Ray Epps <rayharryepps@gmail.com>
2022-08-14 12:13:30 -0600

    v5.1.0 release + series next, previous, first, last

--- src/series.ts
+++ src/series.ts
@@ -1,17 +1,38 @@
 export const series = <T extends string | symbol>(...items: T[]) => {
-  const dict = items.reduce(
+  const { itemsByValue, itemsByIndex } = items.reduce(
     (acc, item, idx) => ({
-      ...acc,
-      [item]: idx,
+      itemsByValue: {
+        ...acc.itemsByValue,
+        [item]: idx,
+      },
+      itemsByIndex: {
+        ...acc.itemsByIndex,
+        [idx]: item,
+      },
     }),
-    {} as Record<T, number>
-  )
+    {
+      itemsByValue: {} as Record<T, number>,
+      itemsByIndex: {} as Record<number, T>,
+    }
+  );
   return {
     min: (a: T, b: T): T => {
-      return dict[a] < dict[b] ? a : b
+      return itemsByValue[a] < itemsByValue[b] ? a : b;
     },
     max: (a: T, b: T): T => {
-      return dict[a] > dict[b] ? a : b
+      return itemsByValue[a] > itemsByValue[b] ? a : b;
     },
-  }
-}
+    first: () => {
+      return itemsByIndex[0];
+    },
+    last: () => {
+      return itemsByIndex[items.length - 1];
+    },
+    next: <TDefault extends null | T> (current: T, defaultValue?: TDefault): T | TDefault => {
+      return itemsByIndex[itemsByValue[current] + 1] ?? defaultValue;
+    },
+    previous: <TDefault extends null | T> (current: T, defaultValue?: TDefault): T | TDefault => {
+      return itemsByIndex[itemsByValue[current] - 1] ?? defaultValue;
+    },
+  };
+};
