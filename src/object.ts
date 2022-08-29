c57a036661bd136e770ac37fe243449b0aab576e
Ray Epps <rayharryepps@gmail.com>
2022-08-28 21:35:00 -0600

    please the mighty gods of lint 🙏

--- src/object.ts
+++ src/object.ts
@@ -163,14 +163,17 @@ export const omit = <T, TKeys extends keyof T>(
 ): Omit<T, TKeys> => {
   if (!obj) return {} as Omit<T, TKeys>
   if (!keys || keys.length === 0) return obj as Omit<T, TKeys>
-  return keys.reduce((acc, key) => {
-    // Gross, I know, it's mutating the object, but we
-    // are allowing it in this very limited scope due
-    // to the performance implications of an omit func.
-    // Not a pattern or practice to use elsewhere.
-    delete acc[key]
-    return acc
-  }, { ...obj })
+  return keys.reduce(
+    (acc, key) => {
+      // Gross, I know, it's mutating the object, but we
+      // are allowing it in this very limited scope due
+      // to the performance implications of an omit func.
+      // Not a pattern or practice to use elsewhere.
+      delete acc[key]
+      return acc
+    },
+    { ...obj }
+  )
 }
 
 export const get = <T, K>(
