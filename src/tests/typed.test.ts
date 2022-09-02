3bb9cf1dce0eb225f2b5cf954911bd9154050502
Ray Epps <ray@unishine.dev>
2022-09-01 19:47:21 -0600

    add isEqual function for deep equality check (#53)
    * add isEqual function
    
    * simplify func and fix test coverage
    
    Co-authored-by: Ray Epps <rayepps@vessel.local>

--- src/tests/typed.test.ts
+++ src/tests/typed.test.ts
@@ -435,4 +435,63 @@ describe('typed module', () => {
     })
   })
 
+  describe('isEqual function', () => {
+    class Person {
+      name: string
+      friends: Person[] = []
+      self?: Person
+      constructor(name: string) {
+        this.name = name
+      }
+    }
+    const jake = new Person('jake')
+    jake.self = jake
+    jake.friends = [jake, jake]
+    const symbolKey = Symbol('symkey')
+    const complex = {
+      num: 0,
+      str: '',
+      boolean: true,
+      unf: void 0,
+      nul: null,
+      obj: { name: 'object', id: 1, chilren: [0, 1, 2] },
+      arr: [0, 1, 2],
+      func() {
+        console.log('function');
+      },
+      loop: null as any,
+      person: jake,
+      date: new Date(0),
+      reg: new RegExp('/regexp/ig'),
+      [symbolKey]: 'symbol'
+    }
+    complex.loop = complex
+    test('returns true for equal things', () => {
+      assert.isTrue(_.isEqual(0, 0))
+      assert.isTrue(_.isEqual('a', 'a'))
+      const hello = Symbol('hello')
+      assert.isTrue(_.isEqual(hello, hello))
+      assert.isTrue(_.isEqual({}, {}))
+      assert.isTrue(_.isEqual(true, true))
+      assert.isTrue(_.isEqual(new RegExp(/a*s/), new RegExp(/a*s/)))
+      const now = new Date()
+      assert.isTrue(_.isEqual(now, now))
+      assert.isTrue(_.isEqual([], []))
+      assert.isTrue(_.isEqual(complex, { ...complex }))
+      assert.isTrue(_.isEqual([complex, complex], [{ ...complex }, { ...complex }]))
+    })
+    test('returns false for non-equal things', () => {
+      assert.isFalse(_.isEqual(0, 1))
+      assert.isFalse(_.isEqual('a', 'b'))
+      assert.isFalse(_.isEqual(new RegExp(/^http:/), new RegExp(/https/)))
+      assert.isFalse(_.isEqual(Symbol('hello'), Symbol('goodbye')))
+      assert.isFalse(_.isEqual({ z: 23 }, { a: 1 }))
+      assert.isFalse(_.isEqual(true, false))
+      assert.isFalse(_.isEqual(new Date(), new Date('2022-09-01T03:25:12.750Z')))
+      assert.isFalse(_.isEqual([], [1]))
+      assert.isFalse(_.isEqual(complex, { ...complex, num: 222 }))
+      assert.isFalse(_.isEqual([complex], [{ ...complex, num: 222 }]))
+    })
+  })
+
 })
--- src/typed.ts
+++ src/typed.ts
@@ -52,3 +52,29 @@ export const isEmpty = (value: any) => {
   const keys = Object.keys(value).length
   return keys === 0
 }
+
+export const isEqual = <TType>(x: TType, y: TType): boolean => {
+  if (Object.is(x, y)) return true
+  if (x instanceof Date && y instanceof Date) {
+    return x.getTime() === y.getTime()
+  }
+  if (x instanceof RegExp && y instanceof RegExp) {
+    return x.toString() === y.toString()
+  }
+  if (
+    typeof x !== 'object' ||
+    x === null ||
+    typeof y !== 'object' ||
+    y === null
+  ) {
+    return false
+  }
+  const keysX = Reflect.ownKeys(x as unknown as object)
+  const keysY = Reflect.ownKeys(y as unknown as object)
+  if (keysX.length !== keysY.length) return false
+  for (let i = 0; i < keysX.length; i++) {
+    if (!Reflect.has(y as unknown as object, keysX[i])) return false
+    if (!isEqual(x[keysX[i]], y[keysX[i]])) return false
+  }
+  return true
+}
