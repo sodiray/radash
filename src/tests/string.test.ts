1cc0e38024ae47e866dbfc7430b4cf7546a302e6
Giulio Caccin <giulio.caccin@scania.com>
2022-09-03 22:19:56 +0200

    add test and fixed comments for strings

--- src/tests/string.test.ts
+++ src/tests/string.test.ts
@@ -1,6 +1,6 @@
-import { assert } from 'chai'
+import {assert} from 'chai'
 import * as _ from '..'
-import { camel, snake, dash } from '../string'
+import {camel, dash, snake} from '../string'
 
 describe('string module', () => {
 
@@ -110,6 +110,17 @@ describe('string module', () => {
   `
 
       assert.equal(result, expected)
+    })
+
+    test('replaces all occurrences given template', () => {
+
+      const tmp = `Hello <name>.`
+      const data = {
+        name: 'Ray'
+      }
+
+      const result = _.template(tmp, data, /<(.+?)>/g)
+      assert.equal(result, `Hello ${data.name}.`)
     })
   })
 
