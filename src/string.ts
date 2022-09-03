ba32c8a5f6d6bcff1367db86a9773001290d777c
Thomas <thomas.javing@regent.se>
2022-09-03 09:50:03 +0200

    Add support for PascalCase

--- src/string.ts
+++ src/string.ts
@@ -50,6 +50,18 @@ export const dash = (str: string): string => {
 }
 
 /**
+ * Joins all string arguments in a Pasacal case fashion
+ *
+ * pascal('hello world')   -> 'HelloWorld'
+ * pascal('va va boom') -> 'VaVaBoom'
+ */
+export const pascal = (str: string): string => {
+  const parts = str?.split(/[\.\-\s_]/).map(x => x.toLowerCase()) ?? []
+  if (parts.length === 0) return ''
+  return parts.map(str => str.charAt(0).toUpperCase() + str.slice(1)).join('')
+}
+
+/**
  * template is used to replace data by name in template strings.
  * The default expression looks for {{name}} to identify names.
  *
