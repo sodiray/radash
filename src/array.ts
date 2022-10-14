a8a1538cffd8c195adcfa228869ef36962e50374
Vlad Chikalkin <djchikalkin@gmail.com>
2022-10-14 09:35:52 +0300

    add shift function to array module (#97)
    * array: add shift function
    
    * array/shift: allow to shift array in a curcular way
    update check to avoid unnecessary calculations
    
    * array/shift: expand and simplify the check
    
    * array/shift: add missing tests
    
    * tests: add missing asserts for commit f8d2478

--- src/array.ts
+++ src/array.ts
@@ -399,3 +399,18 @@ export const diff = <T>(
   )
   return root.filter(a => !bKeys[identity(a)])
 }
+
+/**
+ * Shift array items by n steps
+ * If n > 0 items will shift n steps to the right
+ * If n < 0 items will shift n steps to the left
+ */
+ export function shift<T>(arr: Array<T>, n: number) {
+  if (arr.length === 0) return arr;
+
+  const shiftNumber = n % arr.length;
+
+  if(shiftNumber === 0) return arr;
+
+  return [...arr.slice(-shiftNumber, arr.length), ...arr.slice(0, -shiftNumber)];
+}
