8d03e151c22d34e891ac247851a347ad711ace4b
Ray Epps <rayepps@vessel.local>
2022-11-02 17:27:47 -0600

    fix default value logic

--- src/number.ts
+++ src/number.ts
@@ -2,20 +2,22 @@ export const toFloat = <T extends number | null = number>(
   value: any,
   defaultValue?: T
 ): number | T => {
+  const def = defaultValue === undefined ? 0.0 : defaultValue
   if (value === null || value === undefined) {
-    return defaultValue ?? 0.0
+    return def
   }
   const result = parseFloat(value)
-  return isNaN(result) ? defaultValue ?? 0.0 : result
+  return isNaN(result) ? def : result
 }
 
 export const toInt = <T extends number | null>(
   value: any,
   defaultValue?: T
 ): number | T => {
+  const def = defaultValue === undefined ? 0 : defaultValue
   if (value === null || value === undefined) {
-    return defaultValue ?? 0
+    return def
   }
   const result = parseInt(value)
-  return isNaN(result) ? defaultValue ?? 0 : result
+  return isNaN(result) ? def : result
 }
