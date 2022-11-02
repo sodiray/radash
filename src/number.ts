4aa81faf0e71581099d35a176eef2d2989530be9
Ray Epps <rayepps@vessel.local>
2022-11-02 17:16:46 -0600

    improve typing with generics

--- src/number.ts
+++ src/number.ts
@@ -1,21 +1,21 @@
-export const toFloat = (
+export const toFloat = <T extends number | null = number>(
   value: any,
-  defaultValue: number | null = 0.0
-): number | null => {
+  defaultValue?: T
+): number | T => {
   if (value === null || value === undefined) {
-    return defaultValue
+    return defaultValue ?? (0.0 as T)
   }
   const result = parseFloat(value)
-  return isNaN(result) ? defaultValue : result
+  return isNaN(result) ? defaultValue ?? (0.0 as T) : (result as T)
 }
 
-export const toInt = (
+export const toInt = <T extends number | null>(
   value: any,
-  defaultValue: number | null = 0
-): number | null => {
+  defaultValue?: T
+): number | T => {
   if (value === null || value === undefined) {
-    return defaultValue
+    return defaultValue ?? (0 as T)
   }
   const result = parseInt(value)
-  return isNaN(result) ? defaultValue : result
+  return isNaN(result) ? defaultValue ?? (0 as T) : (result as T)
 }
