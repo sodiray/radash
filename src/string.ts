a6153f98e5e9f3879f422743595314e219723d3c
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-06-27 00:55:41 +0700

    format

--- src/string.ts
+++ src/string.ts
@@ -130,7 +130,10 @@ export const template = (
  * trim('222222__hello__1111111', '12_') // => 'hello'
  * ```
  */
-export const trim = (str: string | null | undefined, charsToTrim: string = ' ') => {
+export const trim = (
+  str: string | null | undefined,
+  charsToTrim: string = ' '
+) => {
   if (!str) return ''
   const toTrim = charsToTrim.replace(/[\W]{1}/g, '\\$&')
   const regex = new RegExp(`^[${toTrim}]+|[${toTrim}]+$`, 'g')
