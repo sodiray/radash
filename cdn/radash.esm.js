34e6e032a680d8bb4dc6fec2f113a74a30f762f8
UnKnoWn <9733856+UnKnoWn-Consortium@users.noreply.github.com>
2023-06-27 00:06:41 +0800

    feat(curry): add isThrottled method to throttle (#293)

--- cdn/radash.esm.js
+++ cdn/radash.esm.js
@@ -547,14 +547,19 @@ const debounce = ({ delay }, func) => {
 };
 const throttle = ({ interval }, func) => {
   let ready = true;
+  let timer = void 0;
   const throttled = (...args) => {
     if (!ready)
       return;
     func(...args);
     ready = false;
-    setTimeout(() => {
+    timer = setTimeout(() => {
       ready = true;
+      timer = void 0;
     }, interval);
+  };
+  throttled.isThrottled = () => {
+    return timer !== void 0;
   };
   return throttled;
 };
