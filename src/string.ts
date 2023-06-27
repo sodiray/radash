c378bd1bc401045ed7d8e5e47b275d2159ce43d5
Ray Epps <ray@unishine.dev>
2023-06-27 07:48:52 -0600

    fix: split number from letters in snake func (#311)

--- src/string.ts
+++ src/string.ts
@@ -37,7 +37,12 @@ export const camel = (str: string): string => {
  * snake('va va-VOOM') -> 'va_va_voom'
  * snake('helloWord') -> 'hello_world'
  */
-export const snake = (str: string): string => {
+export const snake = (
+  str: string,
+  options?: {
+    splitOnNumber?: boolean
+  }
+): string => {
   const parts =
     str
       ?.replace(/([A-Z])+/g, capitalize)
@@ -45,9 +50,12 @@ export const snake = (str: string): string => {
       .map(x => x.toLowerCase()) ?? []
   if (parts.length === 0) return ''
   if (parts.length === 1) return parts[0]
-  return parts.reduce((acc, part) => {
+  const result = parts.reduce((acc, part) => {
     return `${acc}_${part.toLowerCase()}`
   })
+  return options?.splitOnNumber === false
+    ? result
+    : result.replace(/([A-Za-z]{1}[0-9]{1})/, val => `${val[0]!}_${val[1]!}`)
 }
 
 /**
