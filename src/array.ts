1cab6d881778213a34f84ccd1ca4ec7d8806151d
jukanntenn <jukanntenn@outlook.com>
2023-10-18 09:04:14 +0800

    fix checks

--- src/array.ts
+++ src/array.ts
@@ -96,13 +96,16 @@ export const boil = <T>(
  * Sum all numbers in an array. Optionally provide a function
  * to convert objects in the array to number values.
  */
-export function sum<T extends number>(array: readonly T[]): number;
-export function sum<T extends object>(array: readonly T[], fn: (item: T) => number) : number;
-export function sum <T extends object | number>(array: readonly any[], fn?: (item: T) => number): number {
-  return (array || []).reduce(
-    (acc, item) => acc + (fn ? fn(item) : item),
-    0
-  )
+export function sum<T extends number>(array: readonly T[]): number
+export function sum<T extends object>(
+  array: readonly T[],
+  fn: (item: T) => number
+): number
+export function sum<T extends object | number>(
+  array: readonly any[],
+  fn?: (item: T) => number
+): number {
+  return (array || []).reduce((acc, item) => acc + (fn ? fn(item) : item), 0)
 }
 
 /**
