cca29d0b80a40f31bd779cb8c025cfae4751fe89
Stefaan Vandevelde <stefaan.vandevelde.kortrijk@gmail.com>
2023-11-30 20:30:37 +0100

    Added filtering to mapEntries

--- src/object.ts
+++ src/object.ts
@@ -81,10 +81,10 @@ export const mapEntries = <
   toEntry: (key: TKey, value: TValue) => [TNewKey, TNewValue]
 ): Record<TNewKey, TNewValue> => {
   if (!obj) return {} as Record<TNewKey, TNewValue>
-  return Object.entries(obj).reduce((acc, [key, value]) => {
-    const [newKey, newValue] = toEntry(key as TKey, value as TValue)
-    acc[newKey] = newValue
-    return acc
+    return Object.entries(obj).reduce((acc, [key, value]) => {
+      const alteredEntry = toEntry(key as TKey, value as TValue)
+      if (alteredEntry !== undefined) acc[alteredEntry[0]] = alteredEntry[1]
+      return acc
   }, {} as Record<TNewKey, TNewValue>)
 }
 
