b7a8801a37587b0f55e19b798a1634a590a3ba33
Gustavo Perdomo <gperdomor@gmail.com>
2024-05-07 20:52:38 -0400

    fix: fixed group return type

--- src/array.ts
+++ src/array.ts
@@ -8,7 +8,7 @@ import { isArray, isFunction } from './typed'
 export const group = <T, Key extends string | number | symbol>(
   array: readonly T[],
   getGroupId: (item: T) => Key
-): Partial<Record<Key, T[]>> => {
+): Record<Key, T[]> => {
   return array.reduce((acc, item) => {
     const groupId = getGroupId(item)
     if (!acc[groupId]) acc[groupId] = []
